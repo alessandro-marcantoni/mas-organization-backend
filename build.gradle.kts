9750c7aac50083bafa164d0582079488a3907d18
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-12-15 10:08:34 +0100

    chore: dockerize

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,25 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.google.guava:guava:31.1-jre")
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
+    implementation("org.litote.kmongo:kmongo-coroutine:4.8.0")
     implementation("io.vertx:vertx-core:4.3.6")
     implementation("io.vertx:vertx-lang-kotlin:4.3.6")
     implementation("io.vertx:vertx-web:4.3.6")
-    implementation("org.litote.kmongo:kmongo:4.8.0")
+    implementation("org.litote.kmongo:kmongo-async:4.8.0")
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
